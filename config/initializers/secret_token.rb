71b5f510c8f07592dad2fffb15b2df3246af0a7c
Vignesh Prakasam <vprakasam@anpi.com>
2014-05-17 14:17:02 +0530

    gritter

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gritter::Application.config.secret_key_base = '6cb96c6327dd82c1b9d2b54b078d1d79d0029a11251fb299d51915f00c9596d0a13c1db44b5e9d7d9c16f40ba926e9bb9aa930fcbda202cbc8b9bfeba856baf0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gritter::Application.config.session_store :cookie_store, key: '_gritter_session'
